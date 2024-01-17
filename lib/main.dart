7171c2a97e3ea6808e70538d16c1f726d53571d9
Jeyym Santos <santosjl@students.nu-baliwag.edu.ph>
2024-01-18 01:12:43 +0800

    Landing Page done

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,34 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      themeMode: ThemeMode.system,
-      theme: NUGoTheme.lightTheme,
-      darkTheme: NUGoTheme.darkTheme,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Color(0xFF2F3C99),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/icons/landing_logo.png', height: 100),
+              const SizedBox(height: 15),
+              const Text(
+                'The Nationalian Corner',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 20,
+                    fontFamily: 'ClanPro'),
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/icons/synergy.png', height: 30),
+            const SizedBox(height: 100),
+          ],
+        ),
+      ),
     );
   }
 }
